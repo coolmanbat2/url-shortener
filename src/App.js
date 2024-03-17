0614165ed2bb66b8dea50ef63181745558cfc774
coolmanbat2 <thanusun.thayaparan@mail.utoronto.ca>
2024-03-17 09:43:19 -0400

    Made changes to App.css font styling

--- src/App.js
+++ src/App.js
@@ -7,24 +7,24 @@ function App() {
     <div className="App">
       <div className="urlShort">
         <div className="urlShortBox">
-          <h4>Simplify your links, enter your link here.</h4>
+          <h4 className="header">Simplify your links, enter your link here.</h4>
           <form formAction="" className="urlLinkBox">
             <input className="linkInput" />
             <Submit />
           </form>
         </div>
         <div className="urlExplain">
-          <h3>
+          <h4 className="header">
             Better be on the toilet then not be on the toilet and have to go
-          </h3>
-          <p>
+          </h4>
+          <p className="bodyText">
             We'll begin by talking about how it means to waste soap. The liquid
             soap is a nice idea for the house. We can talk about random things
             while watching two and a half men. With this show we are able to see
             new things that do not manage to keep themselves the same as they
             always are if you know what I mean.
           </p>
-          <p>
+          <p className="bodyText">
             This sentence is on a run-on sentence. The random words that are
             typed in this paragraph provides us with a representation of how the
             world will truly feel from there.
