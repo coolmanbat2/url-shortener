19e4d7e865b44e7673d8f74105b8a8050bd691b5
coolmanbat2 <thanusun.thayapara@mail.utoronto.ca>
2024-03-23 09:14:19 -0400

    Updated URLShortBox to connect to backend code.

--- src/components/submit/URLShortBox.jsx
+++ src/components/submit/URLShortBox.jsx
@@ -2,19 +2,37 @@ import React from "react";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import axios from "axios";
+
+const server = "http://localhost:8080/";
 
 function URLShortBox() {
   // Send over information that was given by the user to the server.
-  function sendInfo(props) {
-    var link = document.getElementById('link').value
-    
+  async function sendInfo() {
+    var link = document.getElementById("link").value;
+    try {
+      const response = await axios.post(server + "add-url", {
+        data: {
+          originalLink: link,
+        },
+      });
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
     <Paper
       component="form"
-      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400, borderRadius: 3 }}
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: 400,
+        borderRadius: 3,
+      }}
     >
       <InputBase
         sx={{ ml: 1, flex: 1 }}
@@ -22,8 +40,13 @@ function URLShortBox() {
         inputProps={{ "aria-label": "link" }}
         id="link"
       />
-      <IconButton onClick={sendInfo} type="button" sx={{ p: "10px" }} aria-label="submit">
-        <ArrowForwardIcon/> 
+      <IconButton
+        onClick={sendInfo}
+        type="button"
+        sx={{ p: "10px" }}
+        aria-label="submit"
+      >
+        <ArrowForwardIcon />
       </IconButton>
     </Paper>
   );
