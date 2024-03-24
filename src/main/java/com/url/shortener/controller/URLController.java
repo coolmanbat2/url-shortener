864c30bf044909bcbc7486cf4f74652493858ecd
coolmanbat2 <thanusun.thayapara@mail.utoronto.ca>
2024-03-24 14:21:04 -0400

    Made changes to URL controller, URL model, and app properties regarding SQL Configuration!

--- src/main/java/com/url/shortener/controller/URLController.java
+++ src/main/java/com/url/shortener/controller/URLController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,26 +15,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class URLController {
     URLService urlService;
+    //TODO: Please change this server link when pushing to prod, as the server will fail if server name is not changed.
+    final String serverLink = "http://localhost:3000";
     @Autowired
     URLController(URLService urlService) {
        this.urlService = urlService;
     }
 
+    @CrossOrigin(origins = serverLink)
     @PostMapping("/add-url")
     public ResponseEntity<String> add(@ModelAttribute URL url) {
         try {
-            String result = urlService.addURL(url);
-
-            return new ResponseEntity<>(result, HttpStatus.OK);
+            URL exists = urlService.getURL(url);
+            if (exists != null) {
+                String result = urlService.addURL(url);
+                return new ResponseEntity<>(result, HttpStatus.OK);
+            } else {
+               return new ResponseEntity<>("Already exists in the system.", HttpStatus.CONFLICT);
+            }
 
         } catch (Exception | CannotCreateLinkException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
+    @CrossOrigin(origins = serverLink)
     @GetMapping("/get-url")
     public ResponseEntity<String> get(@ModelAttribute URL url) {
-        URL result = urlService.getURL(url);
-        return new ResponseEntity<>(result.getOriginalLink(), HttpStatus.OK);
+        try {
+            URL result = urlService.getURL(url);
+            return new ResponseEntity<>(result.getOriginalLink(), HttpStatus.OK);
+
+        } catch (NullPointerException exception) {
+           return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
